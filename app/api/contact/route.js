ca3c34e0bb56021b26753c0cdcb32639fcde620c
Ali-9009 <br6332160@gmail.com>
2026-06-19 15:38:43 +0500

    mail set

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -42,23 +42,23 @@ export async function POST(req) {
             );
         }
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp.titan.email",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
         // const transporter = nodemailer.createTransport({
-        //     service: "gmail",
+        //     host: "smtp.titan.email",
+        //     port: 465,
+        //     secure: true,
         //     auth: {
         //         user: process.env.EMAIL_USER,
         //         pass: process.env.EMAIL_PASS,
         //     },
         // });
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
 
         await transporter.sendMail({
             from: `"Cloud Minds Tech" <${process.env.EMAIL_USER}>`,
